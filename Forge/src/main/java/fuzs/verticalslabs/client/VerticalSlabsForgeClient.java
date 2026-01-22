5e02cc6351f2c750ad91a24af1488522b7fe026d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-01-22 11:01:34 +0100

    setup for 1.21.8

--- Forge/src/main/java/fuzs/verticalslabs/client/VerticalSlabsForgeClient.java
+++ Forge/src/main/java/fuzs/verticalslabs/client/VerticalSlabsForgeClient.java
@@ -1,12 +1,8 @@
 package fuzs.verticalslabs.client;
 
-import fuzs.verticalslabs.VerticalSlabs;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.repository.Pack;
+import fuzs.verticalslabs.VerticalSlabs;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
@@ -17,14 +13,5 @@ public class VerticalSlabsForgeClient {
     @SubscribeEvent
     public static void onConstructMod(final FMLConstructModEvent evt) {
         ClientModConstructor.construct(VerticalSlabs.MOD_ID, VerticalSlabsClient::new);
-    }
-
-    @SubscribeEvent
-    public static void onAddPackFinders(final AddPackFindersEvent evt) {
-        if (evt.getPackType() == PackType.CLIENT_RESOURCES) {
-//            evt.addRepositorySource(p_10542_ -> {
-//                p_10542_.accept(Pack.create("test", Component.literal("comedy"), false));
-//            });
-        }
     }
 }
