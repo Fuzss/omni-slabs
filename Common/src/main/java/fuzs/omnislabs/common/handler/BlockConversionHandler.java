21da5ea49a32d4bcadde5c4ffe74a119f5aa1d1a
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-20 17:37:43 +0200

    sync BlockConversionHandler.java

--- Common/src/main/java/fuzs/omnislabs/common/handler/BlockConversionHandler.java
+++ Common/src/main/java/fuzs/omnislabs/common/handler/BlockConversionHandler.java
@@ -2,12 +2,15 @@ package fuzs.omnislabs.common.handler;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import com.google.common.collect.MapMaker;
 import com.google.common.collect.Maps;
 import fuzs.puzzleslib.common.api.block.v1.BlockConversionHelper;
+import fuzs.puzzleslib.common.api.event.v1.AddBlockEntityTypeBlocksCallback;
 import fuzs.puzzleslib.common.api.event.v1.RegistryEntryAddedCallback;
 import fuzs.puzzleslib.common.api.event.v1.core.EventResultHolder;
 import fuzs.puzzleslib.common.api.event.v1.entity.player.PlayerInteractEvents;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -23,16 +26,22 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LevelEvent;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Map;
 import java.util.function.*;
 
 public class BlockConversionHandler {
     private static final BiMap<Block, Block> BLOCK_CONVERSIONS = HashBiMap.create();
+    private static final Map<BlockState, BlockState> BLOCK_STATE_CONVERSIONS_CACHE = new MapMaker().weakKeys()
+            .weakValues()
+            .makeMap();
 
     public static RegistryEntryAddedCallback<Block> onRegistryEntryAdded(Predicate<Block> filter, BiFunction<Block, BlockBehaviour.Properties, Block> factory, String modId) {
         return (Registry<Block> registry, Identifier id, Block block, BiConsumer<Identifier, Supplier<Block>> registrar) -> {
@@ -50,6 +59,14 @@ public class BlockConversionHandler {
 
     public static BiMap<Block, Block> getBlockConversions() {
         return Maps.unmodifiableBiMap(BLOCK_CONVERSIONS);
+    }
+
+    public static AddBlockEntityTypeBlocksCallback onAddBlockEntityTypeBlocks(Holder.Reference<? extends BlockEntityType<?>> blockEntityType) {
+        return (BiConsumer<BlockEntityType<?>, Block> consumer) -> {
+            for (Map.Entry<Block, Block> entry : BLOCK_CONVERSIONS.entrySet()) {
+                consumer.accept(blockEntityType.value(), entry.getValue());
+            }
+        };
     }
 
     public static PlayerInteractEvents.UseBlock onUseBlock(TagKey<Block> unalteredBlocks, BooleanSupplier configOption) {
@@ -138,5 +155,46 @@ public class BlockConversionHandler {
         } else {
             BlockConversionHelper.setBlockForItem(blockItem, newBlock);
         }
+    }
+
+    @Nullable
+    public static BlockState convertToVanillaBlock(@Nullable BlockState blockState) {
+        // mod replacement block coming in, we need to forward the original
+        return applyBlockConversion(blockState, true);
+    }
+
+    @Nullable
+    public static BlockState convertFromVanillaBlock(@Nullable BlockState blockState) {
+        // original block coming in, we need to convert back to our replacement
+        return applyBlockConversion(blockState, false);
+    }
+
+    @Nullable
+    private static BlockState applyBlockConversion(@Nullable BlockState blockState, boolean inverse) {
+        if (blockState != null) {
+            return BLOCK_STATE_CONVERSIONS_CACHE.computeIfAbsent(blockState, applyBlockConversion(inverse));
+        } else {
+            return null;
+        }
+    }
+
+    private static UnaryOperator<BlockState> applyBlockConversion(boolean inverse) {
+        return (BlockState blockState) -> {
+            BiMap<Block, Block> blockConversions = inverse ? BLOCK_CONVERSIONS.inverse() : BLOCK_CONVERSIONS;
+            if (blockState != null && blockConversions.containsKey(blockState.getBlock())) {
+                Block block = blockConversions.get(blockState.getBlock());
+                return copyAllProperties(blockState, block.defaultBlockState());
+            } else {
+                return blockState;
+            }
+        };
+    }
+
+    private static <T extends Comparable<T>, V extends T> BlockState copyAllProperties(BlockState oldBlockState, BlockState newBlockState) {
+        for (Property.Value<?> value : oldBlockState.getValues().toList()) {
+            newBlockState = newBlockState.trySetValue((Property<T>) value.property(), (V) value.value());
+        }
+
+        return newBlockState;
     }
 }
