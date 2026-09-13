e64f867b117a5f10a9de4fda7f45ec843953f276
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-09-13 10:03:52 +0200

    fix block highlight not rendering on Fabric

--- Common/src/main/java/fuzs/omnislabs/common/client/handler/SlabOutlineHandler.java
+++ Common/src/main/java/fuzs/omnislabs/common/client/handler/SlabOutlineHandler.java
@@ -20,30 +20,38 @@ import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class SlabOutlineHandler {
 
     public static EventResult onRenderHighlight(LevelRenderer levelRenderer, Camera camera, GameRenderer gameRenderer, HitResult hitResult, DeltaTracker deltaTracker, PoseStack poseStack, MultiBufferSource multiBufferSource, ClientLevel level) {
         if (hitResult.getType() == HitResult.Type.BLOCK && camera.getEntity() instanceof Player player) {
-            SyncedSlabSettings syncedSlabSettings = ModRegistry.SYNCED_SLAB_SETTINGS_ATTACHMENT_TYPE.getOrDefault(camera.getEntity(),
-                    SyncedSlabSettings.EMPTY);
             BlockPos blockPos = ((BlockHitResult) hitResult).getBlockPos();
             BlockState blockState = player.level().getBlockState(blockPos);
+            SyncedSlabSettings syncedSlabSettings = ModRegistry.SYNCED_SLAB_SETTINGS_ATTACHMENT_TYPE.getOrDefault(camera.getEntity(),
+                    SyncedSlabSettings.EMPTY);
             SlabType slabType = syncedSlabSettings.getSlabType(player, blockState, blockPos, hitResult.getLocation());
             if (slabType != null) {
                 VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.lines());
                 Vec3 cameraPosition = camera.getPosition();
-                double posX = cameraPosition.x();
-                double posY = cameraPosition.y();
-                double posZ = cameraPosition.z();
-                levelRenderer.renderHitOutline(poseStack,
+                double posX = blockPos.getX() - cameraPosition.x();
+                double posY = blockPos.getY() - cameraPosition.y();
+                double posZ = blockPos.getZ() - cameraPosition.z();
+                // Do not use LevelRenderer::renderHitOutline directly; the Fabric callback runs from leading to unexpected behavior.
+                VoxelShape voxelShape = blockState.setValue(RotatedSlabBlock.TYPE, slabType)
+                        .getShape(player.level(), blockPos, CollisionContext.of(player));
+                LevelRenderer.renderShape(poseStack,
                         vertexConsumer,
-                        player,
+                        voxelShape,
                         posX,
                         posY,
                         posZ,
-                        blockPos,
-                        blockState.setValue(RotatedSlabBlock.TYPE, slabType));
+                        0.0F,
+                        0.0F,
+                        0.0F,
+                        0.4F);
+
                 return EventResult.INTERRUPT;
             }
         }
